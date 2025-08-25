99a8445dd9da9a8f33670f097cb1a539acfb670f
S Harris <>
2025-08-25 15:47:16 +0200

    cheri: Modify LLVM wrapper to support CHERI tag options for memcpy and memmove
    Co-authored-by: Edoardo Marangoni <ecmm@anche.no>

--- compiler/rustc_codegen_llvm/src/llvm/ffi.rs
+++ compiler/rustc_codegen_llvm/src/llvm/ffi.rs
@@ -678,6 +678,25 @@ pub(crate) enum Opcode {
     CatchSwitch = 65,
 }
 
+/// LLVMPreserveCheriTags
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub(crate) enum PreserveCheriTags {
+    Unknown,
+    Required,
+    Unnecessary,
+}
+
+impl PreserveCheriTags {
+    pub(crate) fn from_generic(value: rustc_codegen_ssa::common::PreserveCheriTags) -> Self {
+        match value {
+            rustc_codegen_ssa::common::PreserveCheriTags::Unknown => PreserveCheriTags::Unknown,
+            rustc_codegen_ssa::common::PreserveCheriTags::Required => PreserveCheriTags::Required,
+            rustc_codegen_ssa::common::PreserveCheriTags::Unnecessary => PreserveCheriTags::Unnecessary,
+        }
+    }
+}
+
 unsafe extern "C" {
     type Opaque;
 }
@@ -2041,6 +2060,7 @@ unsafe extern "C" {
         Src: &'a Value,
         SrcAlign: c_uint,
         Size: &'a Value,
+        PreserveTags: PreserveCheriTags,
         IsVolatile: bool,
     ) -> &'a Value;
     pub(crate) fn LLVMRustBuildMemMove<'a>(
@@ -2050,6 +2070,7 @@ unsafe extern "C" {
         Src: &'a Value,
         SrcAlign: c_uint,
         Size: &'a Value,
+        PreserveTags: PreserveCheriTags,
         IsVolatile: bool,
     ) -> &'a Value;
     pub(crate) fn LLVMRustBuildMemSet<'a>(
