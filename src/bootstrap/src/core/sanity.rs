0ebe6fea438f6e58f9d6f290990422d0ea66c3e6
Edoardo Marangoni <ecmm@anche.no>
2025-09-02 15:26:44 +0200

    cheri: add the `riscv32cheriot_unknown_cheriotrtos` target
    Note: as of now, (cross-)compilation to
    `riscv32cheriot_unknown_cheriotrtos` is to be considered broken. This
    commit does not introduce it as a complete target, but rather as a
    work-in-progress.

--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -35,7 +35,7 @@ pub struct Finder {
 const STAGE0_MISSING_TARGETS: &[&str] = &[
     "armv7a-vex-v5",
     "riscv64a23-unknown-linux-gnu",
-    // just a dummy comment so the list doesn't get onelined
+    "riscv32cheriot-unknown-cheriotrtos",
     "aarch64_be-unknown-hermit",
     "aarch64_be-unknown-none-softfloat",
 ];
