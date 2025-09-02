9c617a79228dd35d1f43784e0bc043022bdd70bc
Edoardo Marangoni <ecmm@anche.no>
2025-09-02 15:26:44 +0200

    cheri: add the `riscv32cheriot_unknown_cheriotrtos` target
    Note: as of now, (cross-)compilation to
    `riscv32cheriot_unknown_cheriotrtos` is to be considered broken. This
    commit does not introduce it as a complete target, but rather as a
    work-in-progress.

--- compiler/rustc_target/src/spec/mod.rs
+++ compiler/rustc_target/src/spec/mod.rs
@@ -1677,6 +1677,7 @@ supported_targets! {
     ("riscv32imac-esp-espidf", riscv32imac_esp_espidf),
     ("riscv32imafc-esp-espidf", riscv32imafc_esp_espidf),
 
+    ("riscv32cheriot-unknown-cheriotrtos", riscv32cheriot_unknown_cheriotrtos),
     ("riscv32e-unknown-none-elf", riscv32e_unknown_none_elf),
     ("riscv32em-unknown-none-elf", riscv32em_unknown_none_elf),
     ("riscv32emc-unknown-none-elf", riscv32emc_unknown_none_elf),
@@ -1950,6 +1951,7 @@ crate::target_spec_enum! {
         Aix = "aix",
         AmdHsa = "amdhsa",
         Android = "android",
+        CHERIoTRTOS = "cheriotrtos",
         Cuda = "cuda",
         Cygwin = "cygwin",
         Dragonfly = "dragonfly",
@@ -2043,6 +2045,7 @@ crate::target_spec_enum! {
         Abi64 = "abi64",
         AbiV2 = "abiv2",
         AbiV2Hf = "abiv2hf",
+        CHERIoT = "cheriot",
         Eabi = "eabi",
         EabiHf = "eabihf",
         ElfV1 = "elfv1",
@@ -3162,7 +3165,7 @@ impl Target {
             Arch::RiscV32 => {
                 check_matches!(
                     &*self.llvm_abiname,
-                    "ilp32" | "ilp32f" | "ilp32d" | "ilp32e",
+                    "ilp32" | "ilp32f" | "ilp32d" | "ilp32e" | "cheriot",
                     "invalid RISC-V ABI name: {}",
                     self.llvm_abiname,
                 );
