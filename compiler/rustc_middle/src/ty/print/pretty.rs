a545b5377981a6f9d198b33aef7ef39cb7d9bf60
Edoardo Marangoni <ecmm@anche.no>
2025-10-10 14:41:20 +0200

    rustc_middle: replace usages of `pointer_size()` with `pointer_offset()`

--- compiler/rustc_middle/src/ty/print/pretty.rs
+++ compiler/rustc_middle/src/ty/print/pretty.rs
@@ -1836,7 +1836,7 @@ pub trait PrettyPrinter<'tcx>: Printer<'tcx> + fmt::Write {
             }
             // Pointer types
             ty::Ref(..) | ty::RawPtr(_, _) | ty::FnPtr(..) => {
-                let data = int.to_bits(self.tcx().data_layout.pointer_size());
+                let data = int.to_bits(self.tcx().data_layout.pointer_offset());
                 self.typed_value(
                     |this| {
                         write!(this, "0x{data:x}")?;
