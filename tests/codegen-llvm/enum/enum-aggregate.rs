84363556fb69fccc4fa974ac69d83f627d681d6d
Edoardo Marangoni <ecmm@anche.no>
2025-12-21 12:24:09 +0100

    compiletest/codegen-llvm: Add `ADDRSPACE` variable to invocation of FileCheck and make some tests no_std
    ...also add this part of the testsuite in CI.

--- tests/codegen-llvm/enum/enum-aggregate.rs
+++ tests/codegen-llvm/enum/enum-aggregate.rs
@@ -1,12 +1,14 @@
 //@ compile-flags: -Copt-level=0 -Cno-prepopulate-passes
 //@ only-64bit
+//@ ignore-riscv32cheriot-unknown-cheriotrtos Uses i64 for usize
 
 #![crate_type = "lib"]
 #![feature(pattern_types, pattern_type_macro)]
+#![no_std]
 
-use std::cmp::Ordering;
-use std::num::NonZero;
-use std::ptr::NonNull;
+use core::cmp::Ordering;
+use core::num::NonZero;
+use core::ptr::NonNull;
 
 #[no_mangle]
 fn make_some_bool(x: bool) -> Option<bool> {
