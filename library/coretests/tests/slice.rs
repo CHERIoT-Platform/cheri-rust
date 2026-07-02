6750a8da3453fdbc8c8f66cf8c8c10fd6a9cad02
Edoardo Marangoni <ecmm@anche.no>
2026-07-02 16:58:59 +0200

    coretests: remove ignore attribute to test_ptr_from_raw_parts_in_const and test_slice_from_raw_parts_in_const

--- library/coretests/tests/slice.rs
+++ library/coretests/tests/slice.rs
@@ -2498,7 +2498,6 @@ fn test_get_disjoint_mut_range_empty_at_edge() {
 }
 
 #[test]
-#[cfg(not(target_abi = "cheriot"))] // FIXME(cheri): https://github.com/CHERIoT-Platform/cheri-rust/issues/181
 fn test_slice_from_raw_parts_in_const() {
     static FANCY: i32 = 4;
     static FANCY_SLICE: &[i32] = unsafe { std::slice::from_raw_parts(&FANCY, 1) };
