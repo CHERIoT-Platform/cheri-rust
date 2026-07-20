5eda22e1ed3ea159fabe386560471580ac75d4d2
S Harris <>
2026-07-20 12:14:36 +0100

    core doc tests: use pointer type instead of usize to get size of pointer

--- library/core/src/primitive_docs.rs
+++ library/core/src/primitive_docs.rs
@@ -1755,7 +1755,7 @@ mod prim_ref {}
 /// assert_eq!(size_of_val(&not_bar_ptr), 0);
 ///
 /// let bar_ptr: fn(i32) = not_bar_ptr; // force coercion to function pointer
-/// assert_eq!(size_of_val(&bar_ptr), size_of::<usize>());
+/// assert_eq!(size_of_val(&bar_ptr), size_of::<&()>());
 ///
 /// let footgun = &bar; // this is a shared reference to the zero-sized type identifying `bar`
 /// ```
