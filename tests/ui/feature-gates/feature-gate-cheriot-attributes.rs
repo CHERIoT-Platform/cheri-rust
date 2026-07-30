ca0733e7f3edf0c9a11457b049a7b01fb31f1fa0
Edoardo Marangoni <ecmm@anche.no>
2026-07-30 12:25:29 +0200

    tests/ui: add `cheriot-attributes` feature-gate test

---
+++ tests/ui/feature-gates/feature-gate-cheriot-attributes.rs
@@ -0,0 +1,19 @@
+//@ add-minicore
+
+#![feature(no_core, lang_items)]
+#![no_core]
+
+extern crate minicore;
+use minicore::*;
+
+#[repr(C)]
+pub struct Uart {
+    field: u32,
+}
+
+unsafe extern "Rust" {
+    #[cheriot_mmio(name = "uart", permissions = "R")] //~ ERROR  the `#[cheriot_mmio]` attribute is an experimental feature
+    pub static UART0: Uart; //~ ERROR attribute `cheriot_mmio` can be used on CHERIoT targets only
+}
+
+pub fn main() {}
